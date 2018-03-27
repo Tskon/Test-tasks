422c4a07bd216e35214d6607ea7da25ac94077ec
tskon <beatxd@gmail.com>
2018-03-27 23:01:55 +0300

    add basic structure

---
+++ product-card/webpack.config.js
@@ -0,0 +1,103 @@
+//basic vars
+const webpack = require('webpack');
+const path = require('path');
+
+//plugins
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const isProduction = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: ['./app.js', './scss/style.scss']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js'),
+    publicPath: "./js/"
+  },
+  devtool: (isProduction) ? '' : 'source-map',
+  module: {
+    rules: [
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"},
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {sourceMap: true}
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                ident: 'postcss',
+                plugins: (loader) => [
+                  require('autoprefixer')()
+                ]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {sourceMap: true}
+            }
+          ],
+          fallback: "style-loader",
+        })
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          { loader: 'file-loader', options: {publicPath:'../../img', name: '[name]_220x220_1.[ext]'} }
+        ]
+      },
+      // {
+      //   test: /\.svg$/,
+      //   loader: 'svg-url-loader'
+      // }
+    ]
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    open: 'index.html',
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new ExtractTextPlugin("../css/[name].css"),
+    new CopyWebpackPlugin(
+      [
+        {from:'./index.html', to: '../'},
+        {from:'./img', to: '../img'},
+        ],
+      {ignore: [
+        // {glob: 'svg/*'},
+        {glob: 'tmp/*'}
+        ]}
+    )
+  ]
+};
+
+if (isProduction) {
+  module.exports.plugins.push(
+    new UglifyJSPlugin({sourceMap: true})
+  );
+  // module.exports.plugins.push(
+  //   new ImageminPlugin({
+  //     test: /\.(png|jpe?g|dif)$/i
+  //   })
+  // );
+  module.exports.plugins.push(
+    new CleanWebpackPlugin(['dist'])
+  );
+  module.exports.plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize:true
+    })
+  );
+}
